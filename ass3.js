7c7b8cd178f6f8c8f0177fe1810e0a4c6dda6f4d
Raja shree Giriraj <154032352+rajigiriraj@users.noreply.github.com>
2024-06-18 20:17:40 +0530

    Add files via upload

---
+++ ass3.js
@@ -0,0 +1,50 @@
+document.querySelector(".js-go").addEventListener('click',function(e){
+    var input=document.querySelector("input").value;
+    getData(input);
+});
+
+document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+    var input=document.querySelector("input").value;
+    console.log(input)
+    if(e.which === 13) {
+        getData(input);
+    }
+});
+
+
+function getData(data) {
+    let container = document.querySelector(".keyword");
+    container.innerHTML = data;
+    var keyword = data.split(" ").join("+");
+    var url="http://api.giphy.com/v1/gifs/search?q="+keyword+"&api_key=iQvfwH6oEptRSa5EUeHi5hcrGBQ43O9c";
+
+    // AJAX, Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open( 'GET',url);
+    GiphyAJAXCall.send();
+
+    GiphyAJAXCall.addEventListener('load',function(e){
+
+        var data=e.target.response;
+        pom(data);
+    });
+}
+function pom(input){
+    
+    var response = JSON.parse(input);
+    clearResult();
+    var imageUrls = response.data;
+   // var container = document.querySelector(".js-container");
+
+    imageUrls.forEach(function(image){
+        var src = image.images.fixed_height.url;
+        console.log(src);
+
+        var container = document.querySelector(".jscontainer");
+        container.innerHTML +=  "<img src=\""+ src + "\" class=\"jscontainer-image\">";
+});
+}
+function clearResult() {
+    let container = document.querySelector(".jscontainer");
+    container.innerHTML = " ";
+}
